7bdfd5e196f2eae2003a2eee870c3f17bc96049d
Monnine2020 <ethanmonnin@hotmail.com>
2019-09-29 15:42:06 -0400

    Working Pull from API and displaying

--- controller.js
+++ controller.js
@@ -1,5 +1,13 @@
-import gameData from 'fetchAPI.js';
+import gameData from '/fetchAPI.js';
 
-function pullAndShowGame() {
-    data = new gameData();
+function showGame() {
+    document.getElementById("homeTeamScore").innerHTML = data.dataDictionary[0]['HomeTeamScore'];
+    document.getElementById("awayTeamScore").innerHTML = data.dataDictionary[0]['AwayTeamScore'];
+    document.getElementById("homeTeamName").innerHTML = data.dataDictionary[0]['HomeTeamName'];
+    document.getElementById("awayTeamName").innerHTML = data.dataDictionary[0]['AwayTeamName'];
 }
+
+document.getElementById("pullData").onclick = showGame;
+
+var data = new gameData();
+data.dataGen();
--- fetchAPI.js
+++ fetchAPI.js
@@ -1,10 +1,10 @@
 export default class gameData {
 	constructor(){
-		this.data = dataGen();
+		this.dataDictionary;
 	}
-	dataGen() {
-        const response = fetch('https://api.sportsdata.io/v3/cfb/scores/json/GamesByDate/2019-SEP-7?key=9da2786fbdbd46a780193a8126deafb5')
+	async dataGen() {
+        const response = await fetch('https://api.sportsdata.io/v3/cfb/scores/json/GamesByDate/2019-SEP-7?key=9da2786fbdbd46a780193a8126deafb5');
         const scoreDataDictionary = await response.json();
-        return scoreDataDictionary;
+        this.dataDictionary = scoreDataDictionary;
     }
 }
