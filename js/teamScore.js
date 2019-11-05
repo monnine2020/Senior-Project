f66d3a6f998c159215788bd3df0d2eebd9f65d87
Monnine2020 <ethanmonnin@hotmail.com>
2019-11-05 10:19:43 -0500

    Added Tests for teamScore

--- js/teamScore.js
+++ js/teamScore.js
@@ -10,4 +10,7 @@ export default class teamScore {
     getScore() {
         return this.totalScore;
     }
+    getTeamName() {
+        return this.teamName;
+    }
 }
--- test/teamScore.spec.js
+++ test/teamScore.spec.js
@@ -2,5 +2,20 @@ let expect = chai.expect;
 import teamScore from '../js/teamScore.js';
 
 describe('Basic class functionality', () => {
-	it('', () => {});
+	let testTeam = new teamScore("Team One");
+	it('Class object correctly made given a team name', () => {
+		expect(testTeam.teamName).to.equal("Team One");
+		expect(testTeam.totalScore).to.equal(0);
+	});
+
+	it('Class object getters correctly work', () => {
+		expect(testTeam.getTeamName()).to.equal("Team One");
+		expect(testTeam.getScore()).to.equal(0);
+	});
+
+	it('Update Score function works correctly with given winning team', () => {
+		let testGame = {"points": 10, "winningTeam": "Team One"};
+		testTeam.updateScore(testGame);
+		expect(testTeam.getScore()).to.equal(10);
+	});
 });
