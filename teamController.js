1d0cbf524573733734b3ab388e108281a2d22800
Monnine2020 <ethanmonnin@hotmail.com>
2019-11-21 12:47:58 -0500

    Refactor and Readability

--- teamController.js
+++ teamController.js
@@ -11,27 +11,52 @@ function dataCreator() {
 function genDecidedListOfGames() {
     let game;
     for(let gameCursor = 0; gameCursor < Object.keys(data.gameData).length; gameCursor++) {
-        if(data.gameData[gameCursor]["GameEndDateTime"] !== null) {
-            game = data.gameData[gameCursor];
-            scoreAbleGames[gameCursor] = game;
+        if(gameIsComplete(gameCursor)) {
+            generateValidGame(gameCursor);
         }
         else {
-            scoreAbleGames[gameCursor] = "Not Complete";
+            generateNotValidGame(gameCursor);
         }
     } 
+
+
+    function gameIsComplete(gameCursor) {
+        return data.gameData[gameCursor]["GameEndDateTime"] !== null;
+    }
+
+    function generateValidGame(gameCursor) {
+        game = data.gameData[gameCursor];
+        scoreAbleGames[gameCursor] = game;
+    }
+
+    function generateNotValidGame(gameCursor) {
+        scoreAbleGames[gameCursor] = "Not Complete";
+    }
 }
 
 function updateAllScores() {
     let scoredGame;
     let teamName;
     for(let scoredGameCursor = 0; scoredGameCursor < Object.keys(scoredGameLibrary).length; scoredGameCursor++) {
-        if(scoredGameLibrary[scoredGameCursor] !== undefined) {
+        if(gameExists(scoredGameCursor)) {
             scoredGame = scoredGameLibrary[scoredGameCursor];
-            if(scoredGame["winningTeam"] in teamLibrary) {
-                teamName = scoredGame["winningTeam"];
-                teamLibrary[teamName].updateScore(scoredGame);
+            if(teamIsValid()) {
+                updateTeamScore();
             }
         }
+    }
+
+    function gameExists(scoredGameCursor) {
+        return scoredGameLibrary[scoredGameCursor] !== undefined;
+    }
+
+    function updateTeamScore() {
+        teamName = scoredGame["winningTeam"];
+        teamLibrary[teamName].updateScore(scoredGame);
+    }
+
+    function teamIsValid() {
+        return scoredGame["winningTeam"] in teamLibrary;
     }
 }
 
@@ -39,12 +64,20 @@ function scoreAllTeams() {
     let scoredGame;
     let game;
     for(let gameCursor = 0; gameCursor < Object.keys(scoreAbleGames).length; gameCursor++) {
-        if(scoreAbleGames[gameCursor] !== "Not Complete"){
-            game = scoreAbleGames[gameCursor];
-            scoredGame = new gameScorer(game,data.conferenceData);
-            scoredGameLibrary[gameCursor] = scoredGame.score();
+        if(gameIsComplete(gameCursor)){
+            scoreAndStoreGame(gameCursor);
         }
     } 
+
+    function scoreAndStoreGame(gameCursor) {
+        game = scoreAbleGames[gameCursor];
+        scoredGame = new gameScorer(game, data.conferenceData);
+        scoredGameLibrary[gameCursor] = scoredGame.score();
+    }
+
+    function gameIsComplete(gameCursor) {
+        return scoreAbleGames[gameCursor] !== "Not Complete";
+    }
 }
 
 function createAllTeams() {
@@ -53,19 +86,26 @@ function createAllTeams() {
     for(let conferenceCursor = 0; conferenceCursor <= 19; conferenceCursor++) {
         for(let teamCursor = 0; teamCursor <= data.conferenceData[conferenceCursor]['Teams'].length - 1; teamCursor++) {
             teamName = data.conferenceData[conferenceCursor]['Teams'][teamCursor]["School"]+ " " + data.conferenceData[conferenceCursor]['Teams'][teamCursor]["Name"];
-            teamLibrary[teamName] = new teamScore(teamName);
-
-            var opt = document.createElement('option');
-            opt.value = teamName;
-            opt.innerHTML = teamName;
-            selector.add(opt);
+            addTeamToTeamLibrary();
+            updateTeamSelector();
         }  
     } 
+
+
+    function updateTeamSelector() {
+        var opt = document.createElement('option');
+        opt.value = teamName;
+        opt.innerHTML = teamName;
+        selector.add(opt);
+    }
+
+    function addTeamToTeamLibrary() {
+        teamLibrary[teamName] = new teamScore(teamName);
+    }
 }
 
 function getScoreOfTeam() {
     let teamName = document.getElementById("teamSelector").value;
-    console.log(teamName);
     let points = teamLibrary[teamName].getScore();
     document.getElementById("points").innerHTML= "Total Points: " + points;
 }
@@ -84,7 +124,8 @@ window.setTimeout(()=> {
     updateAllScores();
 },2000);
 
-window.setTimeout(()=> {
-    console.log(teamLibrary);
-    console.log(scoredGameLibrary)
-},10000);
+// Debug Code
+// window.setTimeout(()=> {
+//     console.log(teamLibrary);
+//     console.log(scoredGameLibrary)
+// },10000);
